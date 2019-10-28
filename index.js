3fe5c62a60cde61cdd46bf8cf8ecec71278d5de7
Ayomide Jones <ayormeday@gmail.com>
2019-10-28 15:45:28 +0100

    set up functional

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('*', handleDefaultRequest)
+
+function handleDefaultRequest(req, res) {
+  res.json('hello world')
+}
+
+app.listen(process.env.PORT || 3500, () => {
+  console.log('listening on ' + (process.env.PORT || 3500));
+})
